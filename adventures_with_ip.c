9575479af3ed5a32ea7b2d56c0e412be3f144e4a
amaanraz <amaanraz115@gmail.com>
2025-02-26 15:13:29 -0800

    basically globaled for dual core nonsense

--- adventures_with_ip.c
+++ adventures_with_ip.c
@@ -12,6 +12,9 @@
 #define ARM1_STARTADR 0xFFFFFFF0
 #define ARM1_BASEADDR 0x10080000
 #define COMM_VAL (*(volatile unsigned long *)(0xFFFF0000))
+#define AUDIO_SAMPLE_CURRENT_MOMENT (*(volatile unsigned long *)(0xFFFF0001))
+// dow -data C:/Users/tmm12/Desktop/sources/audio_samples/left_list.data 0x018D2008
+// dow -data C:/Users/tmm12/Desktop/sources/audio_samples/left_list_drum.data 0x020BB00C
 
 // Need to make super long to work for some reason
 // So the "record seconds" and playback time isn't fully accurate
@@ -19,6 +22,7 @@
 #define RECORD_SECONDS 35
 #define MAX_SAMPLES (SAMPLES_PER_SECOND * RECORD_SECONDS * 15)
 int NUM_BYTES_BUFFER = 5242880;
+int j = 0; // for drum reset
 
 #include "xscugic.h"
 #include "xil_exception.h"
@@ -37,19 +41,29 @@ int NUM_BYTES_BUFFER = 5242880;
 XGpio LEDInst, BTNInst;
 XScuGic INTCInst;
 
-static int led_data;
+// static int led_data;
 static int btn_value;
 
 // Stereo buffer
 static u32 audio_buffer[MAX_SAMPLES * 2];
 static int recorded_samples = 0;
 static int playing = 0;
+static int playing_drum = 0;
 static int paused = 0;
 static int record_flag = 0;
 static int play_flag = 0;
 static int pause_flag = 0;
+static int drum_flag = 0;
 
-u32 delay_us = 530;
+u32 delay_us = 476;
+
+// access in the core possibly
+int * drum = (int *)0x020BB00C;
+int NUM_SAMPLES_DRUM = 26880;
+int * song = (int *)0x018D2008;
+int NUM_SAMPLES = 1755840;
+
+// u32 delay_us_drum = 60;
 
 //----------------------------------------------------
 // PROTOTYPE FUNCTIONS
@@ -69,26 +83,29 @@ void BTN_Intr_Handler(void *InstancePtr) {
     }
 
     btn_value = XGpio_DiscreteRead(&BTNInst, 1);
-
     if (btn_value == 8) {
-        record_flag = 1;
+    	// Recording functionality
+        // record_flag = 1;
+
+    	// Play/pause functionality
+		// paused = !paused;  // Toggle paused directly
+		// xil_printf("Audio %s.\r\n", paused ? "Paused" : "Resumed");
+
+    	// Drum sounds
+    	drum_flag = 1;
+    	j=0;
     } else if (btn_value == 4) {
         play_flag = 1;
     } else if (btn_value == 16) {
-        //paused = !paused;  // Toggle paused directly
-        //xil_printf("Audio %s.\r\n", paused ? "Paused" : "Resumed");
         delay_us = delay_us + 1;
-
         xil_printf("Delay (us): %d", delay_us);
     } else if(btn_value == 2){
     	if (delay_us > 1){
     		delay_us = delay_us - 1;
     	}
-
         xil_printf("Delay (us): %d", delay_us);
-
     } else if (btn_value == 1){
-    	// center button
+    	// Center button
     	COMM_VAL = 1;
     }
 
@@ -150,39 +167,101 @@ void record_audio() {
 }
 
 void play_audio() {
-    xil_printf("Playing Recording...\r\n");
+    xil_printf("Playing sample from memory...\r\n");
     playing = 1;
     int i = 0;
 
-    while (i < recorded_samples && playing) {
+    while (playing) {
         while (paused) {
             // Stay in this loop until unpaused
             usleep(500);  // Prevent CPU overuse
         }
 
-        // Introduce delay between samples based on the sample rate
-		for(int i = 0; i < delay_us; i++){
-			// nop
+        // Milestone 2 stuff: Add drum sound here inside if statement
+        // Then add drum effects at that point to the song indices
+        int audio_sample = song[i]*5;
+
+        if (drum_flag && j < NUM_SAMPLES_DRUM) {
+           audio_sample += drum[j] * 150;  // Simple addition mixing
+        	 j++;  // Move drum sample forward
+         }
+
+        // TEST: Playing song on top of itself instead of button sound
+        // WORKS!!
+//        if (drum_flag && j < NUM_SAMPLES) {
+//			audio_sample += song[j] * 100;  // Simple addition mixing
+//			j++;  // Move drum sample forward
+//		}
+
+        // write to the global thing for like dual core connection
+        AUDIO_SAMPLE_CURRENT_MOMENT = audio_sample;
+        Xil_Out32(I2S_DATA_TX_L_REG, audio_sample);  // Send left channel
+        Xil_Out32(I2S_DATA_TX_R_REG, audio_sample);  // Send right channel
+
+        i++; // Move to the next left sample for the next iteration
+
+		for(int d=0;d<delay_us;d++){
+			asm("NOP");
 		}
 
-		i += 2;
-        // Send the left and right audio samples to the I2S interface
-        Xil_Out32(I2S_DATA_TX_L_REG, audio_buffer[i * 2]);
-        Xil_Out32(I2S_DATA_TX_R_REG, audio_buffer[i * 2 + 1]);
+		if (i >= NUM_SAMPLES) {
+			// To loop
+ 			// xil_printf("Looping\n");
+ 			// i = 0;  // Reset index to loop through samples
+			// To exit
+			playing = 0;
+		}
 
-        i++;
+		// Stop drum playback if it ends
+		// Can prob get rid of dis
+		if (j >= NUM_SAMPLES_DRUM) {
+			drum_flag = 0;
+			j=0;
+		}
     }
     xil_printf("Playback stopped.\r\n");
-    playing = 0;
     play_flag = 0;
+    drum_flag = 0;
+}
+
+void play_drum() {
+	xil_printf("Playing drum sample from memory...\r\n");
+	playing_drum = 1;
+	int i = 0;
+
+
+	while (playing_drum) {
+		while (paused) {
+			// Stay in this loop until unpaused
+			usleep(500);  // Prevent CPU overuse
+		}
+
+		Xil_Out32(I2S_DATA_TX_L_REG, drum[i]*100);  // Send left channel
+		Xil_Out32(I2S_DATA_TX_R_REG, drum[i]*100);  // Send right channel
+
+		i++; // Move to the next left sample for the next iteration
+
+		for(int j=0;j<delay_us;j++){
+			asm("NOP");
+		}
+
+		if (i >= NUM_SAMPLES_DRUM) {
+			playing_drum = 0;
+		}
+	}
+	xil_printf("Drum effect complete.\r\n");
+	drum_flag = 0;
 }
 
 
 void menu() {
     while (1) {
-        if (record_flag) {
-            record_audio();
-        }
+//        if (record_flag) {
+//            record_audio();
+//        }
+    	if (drum_flag) {
+    		play_drum();
+    	}
         if (play_flag) {
             play_audio();
         }
@@ -193,62 +272,18 @@ void menu() {
     }
 }
 
-// int main(void) {
-//	init_platform();
-//	COMM_VAL = 0;
-//
-//	//Disable cache on OCM
-//	// S=b1 TEX=b100 AP=b11, Domain=b1111, C=b0, B=b0
-//	Xil_SetTlbAttributes(0xFFFF0000,0x14de2);
-//
-//	print("ARM0: writing startaddress for ARM1\n\r");
-//	Xil_Out32(ARM1_STARTADR, ARM1_BASEADDR);
-//	dmb(); //waits until write has finished
-//
-//	print("ARM0: sending the SEV to wake up ARM1\n\r");
-//	sev();
-//
-//
-//    int status;
-//
-//    status = XGpio_Initialize(&BTNInst, BTNS_DEVICE_ID);
-//    if (status != XST_SUCCESS) return XST_FAILURE;
-//
-//    XGpio_SetDataDirection(&BTNInst, 1, 0xFF);
-//
-//    status = IntcInitFunction(INTC_DEVICE_ID, &BTNInst);
-//    if (status != XST_SUCCESS) return XST_FAILURE;
-//
-//    xil_printf("Initializing audio system...\r\n");
-//    IicConfig(XPAR_XIICPS_0_DEVICE_ID);
-//    AudioPllConfig();
-//    AudioConfigureJacks();
-//    xil_printf("Audio system ready.\r\n");
-//
-//    // read into memory the left and right data
-//    int * audio_buffer_pointer = (int *)0x00900000;
-//    int * audio_sample_1 = (int *)0x018D2008;
-//    memcpy(audio_buffer_pointer, audio_sample_1, NUM_BYTES_BUFFER);
-//
-//    xil_printf("Samples saved to memory.\r\n");
-//
-//    menu();
-//    return 0;
-//}
-
-
 int main()
 {
     init_platform();
     COMM_VAL = 0;
 
-    //Disable cache on OCM
+    // Disable cache on OCM
     // S=b1 TEX=b100 AP=b11, Domain=b1111, C=b0, B=b0
     Xil_SetTlbAttributes(0xFFFF0000,0x14de2);
 
     xil_printf("ARM0: writing startaddress for ARM1\n\r");
     Xil_Out32(ARM1_STARTADR, ARM1_BASEADDR);
-    dmb(); //waits until write has finished
+    dmb(); // Waits until write has finished
 
     print("ARM0: sending the SEV to wake up ARM1\n\r");
     sev();
@@ -268,37 +303,6 @@ int main()
 	AudioPllConfig();
 	AudioConfigureJacks();
 	xil_printf("Audio system ready.\r\n");
-
-	// read into memory
-	int * audio_sample_1 = (int *)0x018D2008;
-
-
-	int i = 0;
-	int NUM_SAMPLES = 791552;//(NUM_BYTES_BUFFER / sizeof(int));  // Total number of int samples
-
-	while (1) {
-		while (paused) {
-			// Stay in this loop until unpaused
-			usleep(500);  // Prevent CPU overuse
-		}
-
-
-		Xil_Out32(I2S_DATA_TX_L_REG, audio_sample_1[i]*100);  // Send left channel
-		Xil_Out32(I2S_DATA_TX_R_REG, audio_sample_1[i]*100);  // Send right channel
-
-		i++; // Move to the next left sample for the next iteration
-
-		for(int j=0;j<delay_us;j++){
-			asm("NOP");
-		}
-
-		if (i >= NUM_SAMPLES) {
-			xil_printf("Looping\n");
-			i = 0;  // Reset index to loop through samples
-		}
-	}
-
-	xil_printf("Samples saved to memory.\r\n");
 
 	menu();
 
