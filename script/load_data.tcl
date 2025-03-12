e4f447c34fe7baafac2aab91a870a028e58bdf6a
taaibahm <91577700+taaibahm@users.noreply.github.com>
2025-03-11 17:03:51 -0700

    Add files via upload

---
+++ script/load_data.tcl
@@ -0,0 +1,21 @@
+connect
+
+
+
+# Load the .data file to a specified memory address
+# Replace 0xADDRESS with the actual memory location where you want to store the data
+#dow -data C:\\Users\\sas40\\Desktop\\idlestone3\\smples\\left_list.data 0x018D2008
+#dow -data C:\\Users\\sas40\\Desktop\\idlestone3\\smples\\left_list_snare.data 0x028A4010
+#dow -data C:\\Users\\sas40\\Desktop\\idlestone3\\smples\\left_list_clap.data 0x0308D014
+#dow -data C:\\Users\\sas40\\Desktop\\idlestone3\\smples\\left_list_kickhard.data 0x03876018
+#dow -data C:\\Users\\sas40\\Desktop\\idlestone3\\smples\\left_list_hihat.data 0x0328D014
+
+#dow -data C:\\Users\\tmm12\\Desktop\\sources\\images\\HomepageDJ.data 0x018D2012
+#dow -data C:\\Users\\tmm12\\Desktop\\sources\\images\\HomepageRecord.data 0x088D2012
+dow -data C:\\Users\\tmm12\\Desktop\\sources\\images\\HomepageSelectSample.data 0x028DD00C
+dow -data C:\\Users\\tmm12\\Desktop\\sources\\images\\DJmode.data 0x060BB00C
+#dow -data C:\\Users\\tmm12\\Desktop\\sources\\images\\RecordingMode.data 0x020BB00C
+#dow -data C:\\Users\\tmm12\\Desktop\\sources\\images\\SampleSelect1.data 0x044CB00C
+#dow -data C:\\Users\\tmm12\\Desktop\\sources\\images\\SampleSelect2.data 0x072BB00C
+#dow -data C:\\Users\\tmm12\\Desktop\\sources\\images\\SampleSelect3.data 0x094DB00C
+#dow -data C:\\Users\\tmm12\\Desktop\\sources\\images\\SampleSelectBack.data 0x034D2012
