8844a04ff31f27e08ccc3b45e5aa15dbae46d887
amaanraz <amaanraz115@gmail.com>
2025-03-21 18:08:13 -0700

    Add files via upload

---
+++ audio_samples/load_smples.tcl
@@ -0,0 +1,21 @@
+connect
+
+
+
+# Load the .data file to a specified memory address
+# Replace 0xADDRESS with the actual memory location where you want to store the data
+dow -data C:\\Users\\sas40\\Desktop\\idlestone4\\smples\\sample1.data 0x01300000
+dow -data C:\\Users\\sas40\\Desktop\\idlestone4\\smples\\left_list_snare.data 0x028A4010
+dow -data C:\\Users\\sas40\\Desktop\\idlestone4\\smples\\left_list_clap.data 0x0308D014
+dow -data C:\\Users\\sas40\\Desktop\\idlestone4\\smples\\left_list_kickhard.data 0x0FFFFFC0
+dow -data C:\\Users\\sas40\\Desktop\\idlestone4\\smples\\left_list_hihat.data 0x0328D014
+
+dow -data C:\\Users\\sas40\\Desktop\\idlestone4\\images\\HomepageDJ.data 0x109F2012
+dow -data C:\\Users\\sas40\\Desktop\\idlestone4\\images\\HomepageRecord.data 0x088D2012
+dow -data C:\\Users\\sas40\\Desktop\\idlestone4\\images\\HomepageSelectSample.data 0x028DD00C
+dow -data C:\\Users\\sas40\\Desktop\\idlestone4\\images\\DJmode.data 0x060BB00C
+dow -data C:\\Users\\sas40\\Desktop\\idlestone4\\images\\RecordingMode.data 0x020BB00C
+dow -data C:\\Users\\sas40\\Desktop\\idlestone4\\images\\SampleSelect1.data 0x044CB00C
+dow -data C:\\Users\\sas40\\Desktop\\idlestone4\\images\\SampleSelect2.data 0x072BB00C
+dow -data C:\\Users\\sas40\\Desktop\\idlestone4\\images\\SampleSelect3.data 0x093DB00C
+dow -data C:\\Users\\sas40\\Desktop\\idlestone4\\images\\SampleSelectBack.data 0x034D2012
