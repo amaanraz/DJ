1ff420440e502f737ed47896d723550d4c840e7e
taaibahm <91577700+taaibahm@users.noreply.github.com>
2025-03-28 18:48:26 -0700

    CORRECT MEM ADDRESSES

---
+++ load.tcl
@@ -0,0 +1,21 @@
+connect
+
+
+
+# Load the .data file to a specified memory address
+# Replace 0xADDRESS with the actual memory location where you want to store the data
+dow -data C:\\Users\\tmm12\\Desktop\\DualCore\\Vitis\\smples\\left_list.data 0x01300000
+dow -data C:\\Users\\tmm12\\Desktop\\DualCore\\Vitis\\smples\\left_list_snare.data 0x028A4010
+dow -data C:\\Users\\tmm12\\Desktop\\DualCore\\Vitis\\smples\\left_list_clap.data 0x0308D014
+dow -data C:\\Users\\tmm12\\Desktop\\DualCore\\Vitis\\smples\\left_list_kickhard.data 0x0FFFFFC0
+dow -data C:\\Users\\tmm12\\Desktop\\DualCore\\Vitis\\smples\\left_list_hihat.data 0x0328D014
+
+dow -data C:\\Users\\tmm12\\Desktop\\DualCore\\Vitis\\images\\HomepageDJ.data 0x109F2012
+dow -data C:\\Users\\tmm12\\Desktop\\DualCore\\Vitis\\images\\HomepageRecord.data 0x088D2012
+dow -data C:\\Users\\tmm12\\Desktop\\DualCore\\Vitis\\images\\HomepageSelectSample.data 0x028DD00C
+dow -data C:\\Users\\tmm12\\Desktop\\DualCore\\Vitis\\images\\DJmode.data 0x060BB00C
+dow -data C:\\Users\\tmm12\\Desktop\\DualCore\\Vitis\\images\\RecordingMode.data 0x020BB00C
+dow -data C:\\Users\\tmm12\\Desktop\\DualCore\\Vitis\\images\\SampleSelect1.data 0x044CB00C
+dow -data C:\\Users\\tmm12\\Desktop\\DualCore\\Vitis\\images\\SampleSelect2.data 0x072BB00C
+dow -data C:\\Users\\tmm12\\Desktop\\DualCore\\Vitis\\images\\SampleSelect3.data 0x093DB00C
+dow -data C:\\Users\\tmm12\\Desktop\\DualCore\\Vitis\\images\\SampleSelectBack.data 0x034D2012
